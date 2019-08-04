d5d9c1346fb35461a9635c186af842bac26e73f1
aladdinazhar <50619772+aladdinazhar@users.noreply.github.com>
2019-08-04 10:59:59 +0530

    Update beg17.py

--- beg17.py
+++ beg17.py
@@ -1,12 +1,10 @@
 a,b=map(int,input().split())
 for i in range(a,b):
-    s=0
-    temp=i
-    while(temp>1):
-        d=temp%10
-        s=s+d**3
-        temp=temp//10
-        if(i==s):
-            print(s,end=" ")
-        else:
-            continue        
+  s=0
+  temp=i
+  while(temp>0):
+    c=temp%10
+    s=s+c**3
+    temp=temp//10
+  if(i==s):
+    print(s,end=" ")
