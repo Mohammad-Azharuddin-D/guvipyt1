087499adee2dce7104ad6fd51a0e31790c7adb46
aladdinazhar <50619772+aladdinazhar@users.noreply.github.com>
2019-08-04 09:26:06 +0530

    Create beg16.py

---
+++ beg16.py
@@ -0,0 +1,8 @@
+a,b= map(int,input().split())
+for n in range(a+1,b):
+    if(n>0):
+        for i in range(2,n):
+            if(n%i==0):
+                break
+        else:
+            print(n,end=" ")
